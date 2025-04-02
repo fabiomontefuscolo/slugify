64eb78cb763585ebc660a317f93227b83f324a48
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2025-04-02 23:19:31 +0200

    feat: slugify code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    strings: Vec<String>,
+}
+
+fn slugify(input: String) -> String {
+    let mut result = input.to_lowercase();
+
+    result = result.replace(' ', "-");
+    result = result
+        .chars()
+        .filter(|&c| c.is_alphanumeric() || c == '_' || c == '-')
+        .collect();
+
+    while result.contains("--") {
+        result = result.replace("--", "-");
+    }
+
+    result = result
+        .trim_matches(|c| c == ' ' || c == '-' || c == '_')
+        .to_string();
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    let combined = args.strings.join(" ");
+    let slug = slugify(combined);
+    println!("{}", slug);
 }
